422618b6a733fadfcb156a3d5724b57355290d4c
JohnPaul Swaine <swainejohn301@gmail.com>
2024-07-01 15:54:57 +0100

    initial commit

---
+++ dotnation.js
@@ -0,0 +1,57 @@
+// MORNING
+// console.log("Howdy Partner!".toUpperCase())
+// console.log("Howdy Partner!".toLowerCase())
+// console.log(Math.random()*10)
+// console.log(Math.floor(Math.random()*10))
+// console.log(Math.ceil(Math.random()*10))
+// console.log(Math.round(Math.random()*10))
+// console.log("    |    |    ")
+// console.log("    |    |    ")
+// console.log("    |    |    ")
+// console.log("--------------")
+// console.log("    |    |    ")
+// console.log("    |    |    ")
+// console.log("    |    |    ")
+// console.log("--------------")
+// console.log("    |    |    ")
+// console.log("    |    |    ")
+// console.log("    |    |    ")
+
+// AFTERNOON
+// console.log("All Around The World".charAt(7).toUpperCase())
+// let name ="John"
+// let age = "28"
+// let favouriteDrink = "Cherry Coke"
+// console.log(favouriteDrink)
+// console.log("My favourite drink is " + favouriteDrink)
+// console.log("My name is " + name + ". I am " + age + " years old. My favourite drink is " + favouriteDrink)
+// Activity 1
+// let name = "John"
+// let age = "28"
+// let favcolour = "Royal Blue"
+// console.log("My name is " + name + ". I am currently " + age + ". My favourite colour is " + favcolour + ".")
+// Activity 1 Stretch 
+// let name = "Jonah"
+// let age = "35"
+// let favcolour = "Brown"
+// console.log("My name is " + name + ". I am currently " + age + ". My favourite colour is " + favcolour + ".")
+// Activity 2
+// let breakfast = "nothing"
+// let lunch = "nothing"
+// let tea = "Pizza"
+// console.log("Today for breakfast I ate " + breakfast + ". For lunch I had " + lunch + ". For tea I will eat " + tea + ".")
+// Activity 2 stetch 
+// let breakfast = "nothing"
+// let lunch = "nothing"
+// let tea = "Fish and CHips"
+// console.log("Tomorrow for breakfast I will have " + breakfast + ". I shall then have " + lunch + " for lunch. For tea I will eat " + tea + ".")
+// // Activity 3
+// const today = new Date();
+// const birthday = new Date(2025-5-27)
+// const start = today
+// const end = birthday
+// const elapsed = Math.floor(end-start)/1000;
+// // console.log = Math.floor(end - start)
+// console.log(Elapsed(end - start));
+// return result;
+// Unable to complete activity 3 despite sing MDN as recommended. 
