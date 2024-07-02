e2839b6e5ea5bc88aad5e30c7686ee08842b63ae
JohnPaul Swaine <swainejohn301@gmail.com>
2024-07-02 15:57:53 +0100

    initial commit

---
+++ dotnationday2.js
@@ -0,0 +1,152 @@
+// // MORNING
+// let weather = "dull"
+// if (weather == "sunny") {
+// do this
+//console.log("Well I better wear my sunglasses")
+// }
+// else if (weather == "rainy") {
+// do this
+//console.log("I best not forget a coat")
+// }
+// else {
+// if nothing else matched do this
+//console.log("I best wrap up")
+// }
+// === is strictly equivalent to including the date type. == id equivalent. != Not Equal !== Not strictly equal.
+// let place = "Manchester";
+// let weather = "Cloudy";
+
+// if (place == "Manchester" && weather == "sunny") {
+//     console.log("Check again");
+//     } else if (place == "Manchester" && weather == "Rain") {
+//     console.log("Obviously")
+//     } else if (place == "Manchester" && weather == "Cloudy")
+//     console.log("I best bring a jacket") 
+//     else {
+//     console.log("What is going on? It isn't raining?")
+//     }
+// let day = "saturday"
+//     if (day == "saturday" || day == "sunday") 
+//     { console.log ("It's the weekend!")
+//     } else {
+//     console.log ("Stupid weekday!")
+//     }
+//      ACTIVTY 1
+// let age = 71
+// if (age < 18) {
+//     console.log ("Too young")
+// }
+// else {
+//     console.log("Old enough")
+// }
+
+//      ACTIVITY 1 (STRETCH)
+// let age = "50"
+// let country = "UK"
+// if (age < 18 && country == "UK") {
+//     console.log ("I'm sorry, but you are too young")
+// }
+// else if (age >=18 && country == "UK"){
+//     console.log("What drink would you like")
+// }
+// else {
+//     console.log("Wrong country")
+// }
+
+//      ACTIVITY 2
+// let topping = "pepperoni"
+// switch (topping) {
+//     case "spicy beef":
+//     case "pepperoni":
+//     case "salami":
+//     console.log("I enjoy these toppings on my pizza.");
+//     break;
+//     case "extra cheese":
+//     case "BBQ Base":
+//     console.log("I don't mind these pizza toppings");
+//     break;
+//     case "pineapple":
+//     console.log("Does not belong on pizza");
+//     break;
+//     case "Olives":
+//     case "Artichokes":
+//     console.log ("I hate these toppings on pizza");
+//     default:
+//     console.log ("the topping isn't memorable")
+// }
+//     ACTIVITY 3
+// let password = "everton8"
+// if (password .length >=8)  {
+//     console.log ("Accepted")
+// }
+// else {
+//     console.log ("Password is too short.")
+// }
+//      ACTIVITY 4
+//      ACTIVITY 5
+//      ACTIVITY 6
+// let time = "3"
+// if (time == "8") {
+//     console.log ("en route")
+// }
+// if (time == "7") {
+//     console.log ("Getting ready")
+// }
+// if (time == "9") {
+//     console.log ("arriving at work")
+// }
+//      ACTIVITY 7
+//      ACTIVITY 8
+//      ACTIVITY 9
+
+//      AFTERNOON
+//      ACTIVITY 1
+// let favouritesongs = [
+//     "Lisa - Crossing Field",
+//     "Machico - Fantastic Dreamer",
+//     "Ultra Tower - Kibō no Uta"
+// ]
+// console.log (favouritesongs)
+//      ACTIVITY 1 STRETCH
+// let favouritesongs = [
+//     "Lisa - Crossing Field",
+//     "Machico - Fantastic Dreamer",
+//     "Ultra Tower - Kibō no Uta"
+// ]
+// console.log (favouritesongs)
+// favouritesongs .push ("Takuma Terashima - Nameless Story")
+// favouritesongs .push ("Kay Kyser - [I Got Spurs That] Jingle Jangle Jingle")
+// console.log (favouritesongs)
+
+// favouritesongs .pop()
+// console.log (favouritesongs)
+//      END OF SESSION ACTIVITIES
+//      ACTIVITY 1
+// let favouritemovingpictures=  [
+//     "Sword Art Online: Ordinal Scale",
+//     "Sword Art Online Progressive: Aria of a Starless Night",
+//     "Sword Art Online Progressive: Scherzo of Deep Night",
+//     "KonoSuba: God's Blessing On This Wonderful World: Legend of Crimson",
+//     "Ready Player One"
+// ]
+// console.log (favouritemovingpictures)
+// favouritemovingpictures .push("The Seven Deadly Sins The Movie: Prisoners of the Sky", "The Seven Deadly Sins: Cursed By Light")
+// console.log (favouritemovingpictures)
+// for (let i = 0; i < favouritemovingpictures.length; i++) {
+//     console.log (favouritemovingpictures[i])
+// }
+//      ACTIVITY 2
+// let i = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49"]
+// while (0<i>50) {
+//         i=[Math.floor(Math.random())]
+//         console.log(i)
+// }
+//      ACTIVITY 3
+
+//      ACTIVITY 4
+
+//      ACTIVTY 5
+
+//      ACTIVITY 6
+
+//      ACTIVITY 7
