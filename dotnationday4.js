3faeb336d47eac0b68444960544123b5feb1a78c
JohnPaulSwaine <swainejohn301@gmail.com>
2024-07-04 16:16:04 +0100

    Add files via upload
    Day 4 of Week 2 - Javascript Foundations
    Objects; Algorithms; Flowcharts.
    Contains url for flowchart for afternoon session Activity B

---
+++ dotnationday4.js
@@ -0,0 +1,166 @@
+//      MORNING
+// let person = {
+//     name: "John-Paul",
+//     age: 28,
+// }
+// console.log (person)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person["name"])
+// console.log(person["age"])
+
+// let offer = "none"
+// let time = 1600
+
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     specialOffers: true,
+//     drinks: [
+//         "Cappucino",
+//         "Latte",
+//         "Filtered Coffee",
+//         "Tea",
+//         "Hot Chocolate"
+//     ],
+//     breakfastOffer: "Get a free croissant between 9-11",
+//     lunchOffer: "Get a free 500ml bottle drink with any of our sandwiches between 12:00-14:00",
+//     noOffer: "No offers are available at this time."
+// }
+// cafe.name = "John's Cafe"
+// console.log(cafe)
+// if ((time >= 900) && (time < 1100)) {
+//     offer=cafe.breakfastOffer
+    // console.log(offer)
+// }
+// if ((time >= 1200) && (time <1400)) {
+//     offer=cafe.lunchOffer
+    // console.log(offer)
+// }
+// else {
+// offer=cafe.noOffer
+// console.log(offer)
+// }
+// console.log(offer)
+
+// let day = ""
+// let alarmMessage = ""
+
+// let alarm = {
+//     weekendAlarm: "No alarm necessary.",
+//     weekdayAlarm: "Get up at 7am"
+// }
+// if ( (day == "saturday") || (day == "sunday") ) {
+//     alarmMessage = alarm.weekendAlarm
+// }
+// else {
+//     alarmMessage = alarm.weekdayAlarm
+// }
+// console.log(alarmMessage)
+
+// let person = {
+//     name: "John-Paul",
+//     age: 28,
+//     top5songs: [
+//     "Lisa - Crossing Field",
+//     "Machico - Fantastic Dreamer",
+//     "Ultra Tower - Kibō no Uta",
+//     "Takuma Terashima - Nameless Story",
+//     "Kay Kyser - [I Got Spurs That] Jingle Jangle Jingle",
+//     ]
+// }
+// console.log(person.top5songs)
+// console.log (person)
+
+// let offer = "none"
+// let time = 1600
+
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     specialOffers: false,
+//     drinks: [
+//         "Cappucino",
+//         "Latte",
+//         "Filtered Coffee",
+//         "Tea",
+//         "Hot Chocolate"
+//     ],
+//     breakfastOffer: "Get a free croissant between 9-11",
+//     lunchOffer: "Get a free 500ml bottle drink with any of our sandwiches between 12:00-14:00",
+//     noOffer: "No offers are available at this time.",
+//     opencafe(){
+//         if(this.specialOffers){
+//             return "Time for a special offer!"
+//         }
+//     },
+//     closedcafe(){
+//         return "We are currently closed. We are open between 9am-5pm monday to saturday."
+//     }
+// }
+// cafe.name = "John's Cafe"
+// console.log(cafe.opencafe())
+// if ((time >= 900) && (time < 1100)) {
+//     offer=cafe.breakfastOffer
+//     console.log(offer)
+// }
+// if ((time >= 1200) && (time <1400)) {
+//     offer=cafe.lunchOffer
+//     console.log(offer)
+// }
+// else {
+// offer=cafe.noOffer
+// console.log(offer)
+// }
+// console.log(offer)
+
+// let person = {
+//     name: "John-Paul",
+//     age: 28,
+//     top5songs: [
+//     "Lisa - Crossing Field",
+//     "Machico - Fantastic Dreamer",
+//     "Ultra Tower - Kibō no Uta",
+//     "Takuma Terashima - Nameless Story",
+//     "Kay Kyser - [I Got Spurs That] Jingle Jangle Jingle",
+//     ]
+// }
+// console.log(person.top5songs)
+// console.log (person)
+//      ACTIVITY 1
+// let person = {
+//     myname: "John-Paul",
+//     age: 28,
+//     top5songs: [
+//     "Lisa - Crossing Field",
+//     "Machico - Fantastic Dreamer",
+//     "Ultra Tower - Kibō no Uta",
+//     "Takuma Terashima - Nameless Story",
+//     "Kay Kyser - [I Got Spurs That] Jingle Jangle Jingle",
+//     ],
+//     const sayHi(){
+// console.log(`Hello. My Name is $(myname)`)
+// }
+// }
+// sayHi()
+// console.log(person.top5songs)
+// console.log (person)
+//      ACTIVITY 2
+
+//      ACTIVITY 3
+
+
+//      AFTERNOON
+//  ACTIVITY A
+//  The steps in making a cup of tea are:
+//  1. Add water to the kettle between min and max lines.;
+//  2. Boil kettle.;
+//  3. While kettle is boiling place a teabag into a mug.;
+//  4. If the drinker wants sugar, add the desired amount of sugar to the mug.;
+//  5. Add freshly boiled water.;
+//  6. Stir the teabag and any desired sugar as adding the desired amount of milk.;
+//  7. Remove teabag from mug putting it in the bin.;
+//  8. Serve cup of tea.
+
+//      FLOWCHART
+//  https://lucid.app/lucidchart/dd3ad966-7ecf-4afe-ad0b-2a829272037c/view?page=0_0&invitationId=inv_bfe27f7e-431b-4b4b-b540-6c36a9eb7c71#
